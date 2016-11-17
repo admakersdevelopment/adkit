4618cbba21622ecdff2c0cfdae970082e4b1aa11
Allyson Newby <ally@newby.co.za>
2016-11-17 22:53:28 +0200

    Dev

--- views/site/index.php
+++ views/site/index.php
@@ -6,47 +6,25 @@ $this->title = 'My Yii Application';
 ?>
 <div class="site-index">
 
-    <div class="jumbotron">
-        <h1>Congratulations!</h1>
-
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
-    </div>
-
     <div class="body-content">
 
         <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
+            <div class="col-md-12">
+                <h2>Welcome to Admakers Adkit</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <p>This is an online resource for clients falling under the GMSA Dealership network. The library contains current                       advert examples, previous campaigns and new material - as well as a variety of template-based layouts. All of these ads                 can be customised to suit individual client needs. .</p>
+            	<p>GM Dealers can login and then access all the content.</p>
+            	<p>Once logged in, begin by clicking "Browse all Collections" in the right side menu.</p>
+            	<p>Online briefing: Click on the tab "Sumbit Online Brief" in the top menu.</p>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+            	<h3>Client Service Ambassadors</h3>
+            	<p>OJ van Jaarsveld: <a href="mailto:oj@admakers.com">oj@admakers.com</a> (Account Director)</p>
+            	<p>Danwine Liebenberg: <a href="mailto:danwine@admakers.com">danwine@admakers.com</a> (Account Manager)</p>
+            	<p>Gloria Kula: <a href="mailto:gloria@admakers.com">gloria@admakers.com</a></p>
+            	<p>Neil Britz-Allers: <a href="mailto:neil@admakers.com">neil@admakers.com</a></p>
+            	<p>Zaida Cottle: <a href="mailto:zaida@admakers.com">zaida@admakers.com</a></p>
             </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
-            </div>
+            
         </div>
 
     </div>
