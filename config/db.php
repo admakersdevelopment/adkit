ee0de0843d4ebb5e349ee247c1761df20ac5b5ac
Allyson Newby <ally@newby.co.za>
2017-03-19 21:10:42 +0200

    test

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=adkit',
-    'username' => 'admin',
+    'dsn' => 'mysql:host=localhost;dbname=admak3rs_admakers_adkit_test',
+    'username' => 'admak3rs_admakers_adkit_test',
     'password' => 'admin',
     'charset' => 'utf8',
 ];
